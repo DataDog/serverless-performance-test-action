d27572c74ca6340c75fb324c587a9a7194a02d09
maxday <maxime.david@datadoghq.com>
2023-01-20 17:55:50 -0500

    new bin

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 RUNTIME_ID=$1 #will be used later, nodsjs for now
 LAYER_NAME=$2
-LAYER_NAME=${LAYER_NAME//[-._]} # remove dot as it's a forbiden char for layer name
 
 /build_tools \
     deploy \
