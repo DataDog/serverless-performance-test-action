6b9b23d3f694eac325f3c640a4f47bb007a885d9
maxday <maxime.david@datadoghq.com>
2023-01-20 15:54:20 -0500

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
-echo "runtime = $1"
-echo "layer path = $2"
-pwd
-ls -la
+RUNTIME_ID=$1 #will be used later, nodsjs for now
+LAYER_PATH=$2
+LAYER_NAME=$3
+
+./build-tools/bin/build_tools \
+    deploy \
+    --layer-path "${LAYER_PATH}" \
+    --architecture amd64 \
+    --layer-name "${LAYER_NAME}" \
+    --region "${AWS_REGION}"
