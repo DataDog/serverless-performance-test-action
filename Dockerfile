5407dfc9d56bf9455e623414f215988a054f7828
maxday <maxime.david@datadoghq.com>
2023-01-20 14:55:47 -0500

    test github actions

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,3 @@
-FROM alpine:3.10
+FROM ubuntu:bionic
 COPY entrypoint.sh /entrypoint.sh
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,3 @@
-#!/bin/sh -l
+#!/bin/sh
 echo "maxday log: going to deploy a Lambda function with runtime = $1"
+echo "test env var = $TEST_ENV_VAR"
