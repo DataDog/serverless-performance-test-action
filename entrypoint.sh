76cb833eb5814e977a8ac150d2b2164ce83a23d8
maxday <maxime.david@datadoghq.com>
2023-01-20 15:59:57 -0500

    update path

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,8 @@
 RUNTIME_ID=$1 #will be used later, nodsjs for now
 LAYER_PATH=$2
 LAYER_NAME=$3
-ls -la
-pwd
-./build_tools \
+
+/build_tools \
     deploy \
     --layer-path "${LAYER_PATH}" \
     --architecture amd64 \
