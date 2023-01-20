2fe7bfe867b2129091413fcf8526570a67b908ff
maxday <maxime.david@datadoghq.com>
2023-01-20 17:27:13 -0500

    reset layer

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 RUNTIME_ID=$1 #will be used later, nodsjs for now
 LAYER_NAME=$2
-pwd
-echo "dir 1"
-ls -la
-cd /
-echo "after cd"
-ls -la 
+
 /build_tools \
     deploy \
     --layer-path "/github/workspace/layer.zip" \
