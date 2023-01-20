279888942dc60122e214568fd0e6196333d45ca6
maxday <maxime.david@datadoghq.com>
2023-01-20 15:16:08 -0500

    test add file

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,5 @@
 FROM ubuntu:bionic
+ARG INPUT_LAYER_PATH
 COPY entrypoint.sh /entrypoint.sh
+ADD $INPUT_LAYER_PATH .
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-echo "maxday log: going to deploy a Lambda function with runtime = $1"
-echo "test env var = $TEST_ENV_VAR"
+echo "runtime = $1"
+echo "layer path = $2"
