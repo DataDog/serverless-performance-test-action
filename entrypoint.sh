3a783ceae3da3188d700f18b03a28f1edb4eca83
maxday <maxime.david@datadoghq.com>
2023-01-23 11:07:02 -0500

    add run test param

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 RUNTIME_ID=$1 #will be used later, nodsjs for now
 LAYER_NAME=$2
+RUN_TEST=$4
 
 mv *.zip layer.zip
 
@@ -9,4 +10,13 @@ mv *.zip layer.zip
     --layer-path "/github/workspace/layer.zip" \
     --architecture amd64 \
     --layer-name "${LAYER_NAME}" \
+    --region "${AWS_REGION}"
+
+echo "Run test is :"
+echo $RUN_TEST
+
+/build_tools \
+    deploy_lambda \
+    --layer-name "${LAYER_NAME}" \
+    --runtime "${RUNTIME_ID}" \
     --region "${AWS_REGION}"
