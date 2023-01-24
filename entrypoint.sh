795df4ab1440d03c71a5452be21b5a00569e49b1
maxday <maxime.david@datadoghq.com>
2023-01-24 10:57:51 -0500

    update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,6 @@ LAYER_NAME=$2
 ROLE=$5
 PR_ID=$6
 
-pwd
-cp *.zip /layer.zip
-cd /
-
 /build_tools \
     deploy \
     --layer-path "layer.zip" \
@@ -15,16 +11,11 @@ cd /
     --layer-name "${LAYER_NAME}" \
     --region "${AWS_REGION}"
 
-if [ "$RUN_TEST" = "true" ]
-then
-    /build_tools \
-        deploy_lambda \
-        --layer-name "${LAYER_NAME}" \
-        --runtime "${RUNTIME_ID}" \
-        --region "${AWS_REGION}" \
-        --role "${ROLE}"
-        --pr-id "${PR_ID}"
-        --should-invoke
-else
-    echo "run_test param is not set to 'true', skipping"
-fi
+/build_tools \
+    deploy_lambda \
+    --layer-name "${LAYER_NAME}" \
+    --runtime "${RUNTIME_ID}" \
+    --region "${AWS_REGION}" \
+    --role "${ROLE}"
+    --pr-id "${PR_ID}"
+    --should-invoke
